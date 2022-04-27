3b0dcb896576ccb39e23cf49af17d524dd83877c
DavideManciucca <davidemanciucca@gmail.com>
2022-04-27 23:22:10 +0200

    bonus non completato in quanto non sono completamente sicuro che sia il giusto il procedimento, aspetterò la correzione di stefano

--- script.js
+++ script.js
@@ -1,56 +1,60 @@
-const teamGroup = [
+
+const teamMembers = [
   {
-    nome:'Angela Caroll',
-    role:'chief editor',
-    photo:  "our-team-base\img\angela-caroll-chief-editor.jpg"
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    foto: "img/angela-caroll-chief-editor.jpg"
   },
   {
-    nome:'Angela Lopez',
-    role:'social media manager',
-    photo: " our-team-base\img\angela-lopez-social-media-manager.jpg"
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    foto: "img/angela-lopez-social-media-manager.jpg"
   },
   {
-    nome:'Barbara Ramos',
-    role:'grapich desiner',
-    photo: " our-team-base\img\barbara-ramos-graphic-designer.jpg"
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: "img/wayne-barnett-founder-ceo.jpg"
+  },
+ 
+  {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    foto: "img/walter-gordon-office-manager.jpg"
   },
   {
-    nome:'Scott Estrada',
-    role:'web developer',
-    photo: "our-team-base\img\scott-estrada-developer.jpg"
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    foto: "img/scott-estrada-developer.jpg"
   },
   {
-    nome:'Walter Gordon',
-    role:'office manager',
-    photo: "our-team-base\img\walter-gordon-office-manager.jpg"
-  },
-  {
-    nome:'Wayne Barnett',
-    role:'founder ceo',
-    photo: "our-team-base\img\wayne-barnett-founder-ceo.jpg"
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    foto: "img/barbara-ramos-graphic-designer.jpg"
   },
 ];
-
-let card = document.getElementById('team-container');
-
+const card = document.querySelector('.team-container');
 startFunction();
 
-function startFunction(){
-  for(let i in teamGroup){
-   const output= 
-    ` <div class="team-card">
-    <div class="card-image">
-      <img
-        src="${teamGroup[i].photo}"
-        alt="${teamGroup[i].nome}"
-      />
-    </div>
-    <div class="card-text">
-      <h3>${teamGroup[i].nome}</h3>
-      <p>${teamGroup[i].role}</p>
-    </div>
-  </div> `;
-  document.getElementById('team-contaimner').innerHTML += output;
-  }
+function startFunction() {
+ for(let i in teamMembers){
+  outputCard(i)
+ }
 }
 
+function outputCard(i) {
+  card.innerHTML += 
+ `
+    <div class="team-card">
+      <div class="card-image">
+        <img
+          src= ${teamMembers[i].foto}
+          alt="${teamMembers[i].nome}"
+        />
+      </div>
+      <div class="card-text">
+        <h3>${teamMembers[i].nome}</h3>
+        <p>${teamMembers[i].ruolo}</p>
+      </div>
+    </div>
+ `; 
+}
